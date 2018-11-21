3755ef4ef63ab0211dd56c2b2e3e6d11d4f675da
maggie0408 <1210590780@qq.com>
2018-11-21 13:14:01 +0800

    delete

--- index.js
+++
@@ -1,6 +0,0 @@
-const index=require("./public/js/index");
-const header=require('./public/js/header');
-const footer=require('./public/js/footer');
-const products=require('./public/js/products');
-const signIn=require('./public/js/signin');
-const details=require('./public/js/details');
