60ef033873096629b4b948c15f99c799ca176b7e
maggie0408 <1210590780@qq.com>
2018-11-21 13:15:50 +0800

    1

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const jqueryMin=require('../public/js/jquery.min');
+const popperMin=require('../public/js/popper.min');
+const bootstrapMin=require('../public/js/bootstrap.min');
+const jquery1=require('../public/js/jquery-1.11.3');
+const ajax=require('../public/js/ajax');
+
+const index=require("../public/js/index");
+const header=require('../public/js/header');
+const footer=require('../public/js/footer');
+const products=require('../public/js/products');
+const signIn=require('../public/js/signin');
+const details=require('../public/js/details');
+
+const indexCss=require('../public/css/index.css');
+const baseCss=require('../public/css/base.css');
+const bootstrapCss=require('../public/css/bootstrap.css');
+
